f27577047032b4f95bf6b4666eba1b80755113b5
jitin8 <jitin.thomas8@gmail.com>
2016-04-12 14:20:15 +0200

    Added rotation
    added sample of rotation of robot by pressing R key

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour {
 
-    public float rotationSpeed = 5f;
+	public float rotationSpeed = 45f;
 
-	
+	public bool turn = false;
+
+
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown("space")){
-            transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
-        }
-    }
+		if (Input.GetKeyDown("r"))
+		{
+			turn = !turn;
+		}
+
+		if (turn) { 
+			transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+		}
+	}
 }
